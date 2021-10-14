eb97158a77214af3855356b205626d056281e6fc
Maja <maja.witkowska1703@gmail.com>
2021-10-14 19:49:30 +0200

    zadanie 1 i 2

---
+++ kurs.py
@@ -0,0 +1,17 @@
+
+a = 10
+h = 5
+
+pole_trojkata = a*h/2
+
+print("pole trojkata:", pole_trojkata)
+
+
+Imie = "Maja"
+Wzrost = 183
+print("Mam na imię", Imie, "mam", Wzrost, "cm wzrostu")
+
+import math
+r = 7
+pole_kola = math.pi*r**2
+print("Pole kola to:", pole_kola)
